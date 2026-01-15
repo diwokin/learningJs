297d9a119d35861de9d9e918dda7bf4b729d6a34
diwokin <edwardmaruss@proton.me>
2026-01-15 11:14:19 +0200

    added eksis

---
+++ eksis/3_DALA_20_21/OOO/lapa/skripts.js
@@ -0,0 +1,14 @@
+﻿
+var krasa = "blue";
+function mirgo() {
+    if( krasa == "blue" )
+        krasa = "red";
+    else
+        krasa = "blue";
+    document.getElementById("virsraksts").style.color = krasa;
+}
+
+function funkcija() {
+    document.getElementById("virsraksts").innerHTML = "Eksāmens 2020";
+    setInterval( mirgo, 1000 );
+}
